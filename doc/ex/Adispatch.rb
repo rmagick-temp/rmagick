6b3628d8fdf05613b5a493a32f0dd52414279ae9
tim <tim>
2003-07-10 11:43:42 +0000

    Change output file to 'pixels-array'

--- doc/ex/Adispatch.rb
+++ doc/ex/Adispatch.rb
@@ -6,12 +6,12 @@ pixels = f.dispatch(0,0,f.columns,f.rows,"RGB")
 
 # Write the pixels to a file, to be included
 # in the constitute.rb example.
-File.open('pixels.rb', 'w') { |txt|
+File.open('pixels-array', 'w') { |txt|
     txt.puts("Width = #{f.columns}")
     txt.puts("Height = #{f.rows}")
     txt.print('Pixels = [')
     pixels = pixels.join(',')
-    pixels.gsub!(/(\d+,){1,10}/) { "#{$&}\n" }
+    pixels.gsub!(/(\d+,){1,25}/) { "#{$&}\n" }
     txt.print(pixels)
     txt.puts(']')
 }

373ac0b44f46a7765a917eafeabc42618f430d3b
rmagick <rmagick>
2003-12-06 17:03:33 +0000

    Rewrite to create animated GIF.

--- doc/ex/drop_shadow.rb
+++ doc/ex/drop_shadow.rb
@@ -1,10 +1,16 @@
 #! /usr/local/bin/ruby -w
 require 'RMagick'
 
-# Add a drop shadow to a text string
+# Add a drop shadow to a text string. This example
+# uses a 3-image animation to show each step of the
+# process
 
-Rows = 110
-Cols = 435
+Rows = 60
+Cols = 250
+Text = 'Ruby rocks!'
+
+# This imagelist will contain the animation frames
+anim = Magick::ImageList.new
 
 ex = Magick::Image.new(Cols, Rows)
 
@@ -13,26 +19,42 @@ ex = Magick::Image.new(Cols, Rows)
 
 text = Magick::Draw.new
 text.gravity = Magick::CenterGravity
-text.pointsize = 70
+text.pointsize = 36
 text.font_weight = Magick::BoldWeight
 text.font_style = Magick::ItalicStyle
 text.stroke = 'transparent'
 
 # Draw the shadow text first. The color is a very light gray.
 # Position the text to the right and down.
-text.annotate(ex, 0,0,3,3, 'RMagick') {
-    self.fill = 'gray70'
+text.annotate(ex, 0,0,2,2, Text) {
+    self.fill = 'gray60'
     }
 
-# Blur the shadow.
-ex = ex.gaussian_blur(0,5)
+# Save the first frame of the animation.
+anim << ex
+
+# Blur the shadow. Save a copy of the image as the 2nd frame.
+ex = ex.blur_image(0,3)
+anim << ex.copy
 
 # Add the foreground text in solid black. Position it
 # to the left and up from the shadow text.
-text.annotate(ex, 0,0,-3,-3, 'RMagick') {
-    self.fill = 'black'
+text.annotate(ex, 0,0,-1,-1, Text) {
+    self.fill = 'maroon'
     }
 
-#ex.display
-ex.write('drop_shadow.gif')
+# Save yet another copy of the image as the 3rd frame.
+anim << ex.copy
+
+# Set the delay between frames to 1 second.
+anim.delay = 100
+
+# Set the delay after the last frame to 2 seconds.
+anim.cur_image.delay = 200
+
+# Iterate forever.
+anim.iterations = 0
+
+#anim.animate
+anim.write('drop_shadow.gif')
 exit

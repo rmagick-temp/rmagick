0e526e96b6cc6c8f773235803132f2264be0c996
rmagick <rmagick>
2004-02-08 19:42:28 +0000

    Add warning for RUby < 1.8.0, this can take a loooong time to run.

--- doc/ex/Zconstitute.rb
+++ doc/ex/Zconstitute.rb
@@ -1,10 +1,15 @@
 #! /usr/local/bin/ruby -w
 require 'RMagick'
 
+$stdout.sync = true
+if RUBY_VERSION < '1.8.0'
+    puts 'This example can take several minutes to run...'
+else
+    puts 'This may take a few seconds...'
+end
 load 'pixels-array'
 
-puts 'This may take a few seconds...'
 image = Magick::Image.constitute(Width, Height, "RGB", Pixels)
-#image.display
+
 image.write("constitute.jpg")
 exit

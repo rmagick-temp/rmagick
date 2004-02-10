95483784c86f1f1387b1b805be3f9c8197cbd534
rmagick <rmagick>
2004-02-10 22:51:50 +0000

    Add ImageMagickError handler for the "not implemented" case.

--- doc/ex/bilevel_channel.rb
+++ doc/ex/bilevel_channel.rb
@@ -5,7 +5,16 @@ include Magick
 
 img = Image.read('images/Flower_Hat.jpg').first
 
-img = img.bilevel_channel(2*MaxRGB/3, RedChannel)
-img.write('bilevel_channel.jpg')
+begin
+
+result = img.bilevel_channel(2*MaxRGB/3, RedChannel)
+
+# Substitute the standard "Not Implemented" image
+rescue NotImplementedError
+    result = Magick::Image.read("images/notimplemented.gif").first
+    result.resize!(img.columns, img.rows)
+end
+
+result.write('bilevel_channel.jpg')
 exit
 
--- doc/ex/negate_channel.rb
+++ doc/ex/negate_channel.rb
@@ -5,7 +5,15 @@ require 'RMagick'
 
 img = Magick::Image.read('images/Flower_Hat.jpg').first
 
-img = img.negate_channel(false, Magick::GreenChannel)
+begin
 
-img.write('negate_channel.jpg')
+result = img.negate_channel(false, Magick::GreenChannel)
+
+# Substitute the standard "Not Implemented" image
+rescue NotImplementedError
+    result = Magick::Image.read("images/notimplemented.gif").first
+    result.resize!(img.columns, img.rows)
+end
+
+result.write('negate_channel.jpg')
 exit

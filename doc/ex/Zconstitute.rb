5ddcb9be05a26757e723ce5d5ecfb13f0ccad48e
rmagick <rmagick>
2004-02-11 00:47:22 +0000

    Take out all the "this can take a long time" messages. With the new 'pixels-array' format this script runs very quickly.

--- doc/ex/Zconstitute.rb
+++ doc/ex/Zconstitute.rb
@@ -1,12 +1,6 @@
 #! /usr/local/bin/ruby -w
 require 'RMagick'
 
-$stdout.sync = true
-if RUBY_VERSION < '1.8.0'
-    puts 'This example can take several minutes to run...'
-else
-    puts 'This may take a few seconds...'
-end
 load 'pixels-array'
 
 image = Magick::Image.constitute(Width, Height, "RGB", Pixels)

9be374b3dcd0af4657ccf66ffbde6daf145e8fcf
rmagick <rmagick>
2004-02-11 00:45:29 +0000

    Generate 'pixels-array' file such that the Pixels array is built by concatenating many small arrays. This makes the Zconstitute.rb script run _much_ faster in Ruby 1.6.

--- doc/ex/Adispatch.rb
+++ doc/ex/Adispatch.rb
@@ -5,8 +5,14 @@ f = Magick::Image.read("images/Flower_Hat.jpg").first
 
 pixels = f.dispatch(0,0,f.columns,f.rows,"RGB")
 
-# Write the pixels to a file, to be included
-# in the constitute.rb example.
+# Write the pixels to a file, to be loaded in the Zconstitute.rb
+# example.  Ruby 1.6.8 # loads the Pixels array much faster if we break
+# the array into many small pieces and concatenate them together, so this
+# program generates such a sequence.
+
+first = true
+total = pixels.length
+
 File.open('pixels-array', 'w') { |txt|
     txt.puts("Width = #{f.columns}")
     txt.puts("Height = #{f.rows}")
@@ -16,7 +22,22 @@ File.open('pixels-array', 'w') { |txt|
         txt.printf("%3d,", p)
         x = x.succ
         txt.printf("\n") if x % 25 == 0
+        if x % 1000 == 0
+            if first
+                txt.puts(']')
+                first = false
+            else
+                txt.puts('])')
+            end
+            txt.print('Pixels.concat([')
+        end
     end
-    txt.puts(']')
+
+    if first
+        txt.puts(']')
+        first = false
+    else
+        txt.puts('])')
+    end
 }
 exit

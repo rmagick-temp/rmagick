91af2872c458b6469033ff72308288454a69034d
rmagick <rmagick>
2004-08-21 14:13:16 +0000

    Update to use new arg list

--- doc/ex/splice.rb
+++ doc/ex/splice.rb
@@ -5,9 +5,6 @@ include Magick
 # Demonstrate the splice method.
 
 img = Image.read('images/Flower_Hat.jpg').first
-
-geom = Geometry.new(20,20, img.columns/2, img.rows/2)
-
-spliced_img = img.splice(geom, 'red')
+spliced_img = img.splice(img.columns/2, img.rows/2, 20, 20, 'gray80')
 spliced_img.write('splice.jpg')
 

cf7e0fea1228c0d1cf8cfcdee85d4af3ae7d91d1
rmagick <rmagick>
2004-11-22 01:26:19 +0000

    Change to conform to new crop behavior

--- doc/ex/text_antialias.rb
+++ doc/ex/text_antialias.rb
@@ -26,6 +26,7 @@ gc.draw(imgl)
 # easily see the image, then
 # crop to a representative portion.
 imgl.resize!(3).crop!(235,270, 365,180)
+imgl.page = Magick::Rectangle.new(365, 180)
 imgl.border!(1,1,"black")
 imgl.write("text_antialias.gif")
 exit

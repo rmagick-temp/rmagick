9805c3ddec189753ea626d356b635ed33511196c
rmagick <rmagick>
2004-12-05 22:09:38 +0000

    Replace use of #opacity attribute (which doesn't seem to be working) with semi-transparent background color.

--- examples/image_opacity.rb
+++ examples/image_opacity.rb
@@ -1,28 +1,28 @@
 
-# Use the Image#opacity attribute to create 
-# a semi-transparent title for an image.
- 
+# Ccreate a semi-transparent title for an image.
+
 require 'RMagick'
 include Magick
 
 puts <<END_INFO
 
-This example uses the opacity attribute to create a semi-transparent title.
+This example uses a semi-transparent background color to create a title.
 View the resulting image by entering the command: display image_opacity.miff
 
 END_INFO
 
 balloons = Image.read('../doc/ex/images/Hot_Air_Balloons_H.jpg').first
-legend = Image.new(160, 50) { self.background_color = 'white' }
+legend = Image.new(160, 50) { self.background_color = '#ffffffc0' }
 
 gc = Draw.new
 gc.annotate(legend, 0, 0, 0, 0, "Balloon Day!\\nFri May 2 2003") {
   self.gravity = CenterGravity
   self.stroke = 'transparent'
+  self.fill = 'white'
+  self.pointsize = 18
 }
 
-legend.opacity = 0.50*TransparentOpacity
-result = balloons.composite(legend, NorthWestGravity, OverCompositeOp)
+result = balloons.composite(legend, SouthGravity, OverCompositeOp)
 
 puts "...Writing image_opacity.miff"
 result.write 'image_opacity.miff'

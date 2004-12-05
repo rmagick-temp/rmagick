fc6ddec9f5d48a7f534e8312a273fb7688ff2328
rmagick <rmagick>
2004-12-05 23:29:05 +0000

    Handle NotImplementedError

--- doc/ex/get_multiline_type_metrics.rb
+++ doc/ex/get_multiline_type_metrics.rb
@@ -16,28 +16,38 @@ gc.annotate(background, 0, 0, 0, 0, TEXT) do |gc|
     gc.stroke = 'none'
 end
 
-# Get the metrics
-metrics = gc.get_multiline_type_metrics(background, TEXT)
+begin
 
-# Compute the corners for a rectangle surrounding the text
-x = (background.columns - metrics.width) / 2
-y = (background.rows - metrics.height) / 2
+    # Get the metrics
+    metrics = gc.get_multiline_type_metrics(background, TEXT)
 
-# Draw 2 rectangles over the text.
-gc = Draw.new
-gc.stroke('red')
-gc.stroke_width(5)
-gc.stroke_linejoin('round')
-gc.fill('cyan')
-gc.fill_opacity(0.10)
-gc.rectangle(x, y, x+metrics.width, y+metrics.height)
+    # Compute the corners for a rectangle surrounding the text
+    x = (background.columns - metrics.width) / 2
+    y = (background.rows - metrics.height) / 2
 
-gc.stroke('white')
-gc.stroke_width(1)
-gc.fill('none')
-gc.rectangle(x, y, x+metrics.width, y+metrics.height)
-gc.draw(background)
+    # Draw 2 rectangles over the text.
+    gc = Draw.new
+    gc.stroke('red')
+    gc.stroke_width(5)
+    gc.stroke_linejoin('round')
+    gc.fill('cyan')
+    gc.fill_opacity(0.10)
+    gc.rectangle(x, y, x+metrics.width, y+metrics.height)
 
-background.border!(1,1, 'blue')
+    gc.stroke('white')
+    gc.stroke_width(1)
+    gc.fill('none')
+    gc.rectangle(x, y, x+metrics.width, y+metrics.height)
+    gc.draw(background)
+
+    background.border!(1,1, 'blue')
+
+# Substitute the standard "Not Implemented" image
+rescue NotImplementedError
+    not_implemented = Magick::Image.read("images/notimplemented.gif").first
+    not_implemented.resize!(background.columns, background.rows)
+    background = not_implemented
+end
+
 background.write('get_multiline_type_metrics.gif')
 

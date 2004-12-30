2acd7c93baf73aaa82f576f35690ec12b85da2d1
rmagick <rmagick>
2004-12-30 13:48:06 +0000

    Add borders to the shadow example

--- doc/ex/shadow.rb
+++ doc/ex/shadow.rb
@@ -1,6 +1,7 @@
 require 'RMagick'
 include Magick
 
+# Draw a big red Bezier curve on a transparent background.
 img = Image.new(340, 120) {self.background_color = 'none'}
 gc = Draw.new
 gc.fill('none')
@@ -10,9 +11,21 @@ gc.stroke_width(10)
 gc.bezier(20, 60, 20,-90, 320,210, 320,60)
 gc.draw(img)
 
-img.write('shadow_before.gif')
+# Composite it onto a white background, draw a border around the result,
+# write it to the "before" file.
+before = img.copy
+bg = Image.new(before.columns, before.rows) {self.background_color='white'}
+before = bg.composite(before, CenterGravity, OverCompositeOp)
+before.border!(1,1,'gray80')
+before.write('shadow_before.gif')
+
+# Create the shadow.
 shadow = img.shadow()
+
+# Composite the original image over the shadow, composite the result
+# onto a white background, add a border, write it to the "after" file.
 shadow = shadow.composite(img, NorthWestGravity, OverCompositeOp)
 bg = Image.new(shadow.columns, shadow.rows) {self.background_color='white'}
-shadow = bg.composite(shadow, CenterGravity, OverCompositeOp)
-shadow.write('shadow_after.gif')
+after = bg.composite(shadow, CenterGravity, OverCompositeOp)
+after.border!(1,1,'gray80')
+after.write('shadow_after.gif')

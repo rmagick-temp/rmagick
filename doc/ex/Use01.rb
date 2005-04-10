d79bc17e4835bd7959d38dde33f0e5015128f634
rmagick <rmagick>
2005-04-10 23:53:30 +0000

    New

---
+++ doc/ex/Use01.rb
@@ -0,0 +1,17 @@
+require 'rvg/rvg'
+include Magick
+
+RVG.dpi = 90
+
+rvg = RVG.new(10.cm, 3.cm).viewbox(0, 0, 100, 30) do |canvas|
+    canvas.background_fill = 'white'
+    canvas.desc = "Example Use01 - Simple case of 'use' on a 'rect'"
+    r = RVG::Group.new do |grp|
+            grp.rect(60, 10)
+        end
+    canvas.rect(99.6, 29.6, 0.1, 0.1).styles(:fill=>'none', :stroke=>'blue', :stroke_width=>0.2)
+    canvas.use(r, 20, 10)
+end
+
+rvg.draw.write('Use01.gif')
+
---
+++ doc/ex/Use02.rb
@@ -0,0 +1,22 @@
+require 'rvg/rvg'
+include Magick
+
+RVG.dpi = 90
+
+rvg = RVG.new(10.cm, 3.5.cm).viewbox(0, 0, 100, 35) do |canvas|
+    canvas.background_fill = 'white'
+    canvas.desc = "Example Use02 - Chain 'styles' to 'use'"
+    r = RVG::Group.new do |grp|
+            grp.rect(60, 10).styles(:fill=>'yellow')
+        end
+    canvas.rect(99.6, 34.6, 0.1, 0.1).styles(:fill=>'none', :stroke=>'blue', :stroke_width=>0.2)
+
+    # Since the rectangle specified the fill color the :fill style is ignored here.
+    # However, since the rectangle did not specify a stroke color, the :stroke style
+    # specified here is respected.
+    canvas.use(r, 20, 5).styles(:fill=>'green', :stroke=>'red')
+    canvas.use(r, 20, 20).styles(:fill=>'green', :stroke=>'blue')
+end
+
+rvg.draw.write('Use02.gif')
+

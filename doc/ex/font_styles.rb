69080484fbb2bc2fbb6e524e744ae530c9f7351f
rmagick <rmagick>
2005-04-19 23:23:50 +0000

    New

---
+++ doc/ex/font_styles.rb
@@ -0,0 +1,29 @@
+#! /usr/local/bin/ruby -w
+require 'rvg/rvg'
+include Magick
+
+
+rvg = RVG.new(400, 300) do |canvas|
+    canvas.background_fill = 'white'
+
+    canvas.g do |grp|
+        grp.text(20, 20, "default size")
+        grp.text(20, 40, ":font_size=>14").styles(:font_size=>14)
+        grp.text(20, 60, ":font_size=>16").styles(:font_size=>16)
+        grp.text(20, 90, ":font_size=>24").styles(:font_size=>24)
+    end
+
+    canvas.g.styles(:font_size=>24) do |grp|
+        grp.text(18, 120, ":font_family=>'Courier'").styles(:font_family=>'Courier')
+        grp.text(20, 150, ":font_weight=>'bold'").styles(:font_weight=>'bold')
+        grp.text(20, 180, ":font_stretch=>'normal'").styles(:font_stretch=>'normal')
+        grp.text(20, 210, ":font_stretch=>'condensed'").styles(:font_stretch=>'condensed')
+        grp.text(20, 240, ":font_style=>'italic'").styles(:font_style=>'italic')
+        grp.text(20, 270, ":font_weight=>900").styles(:font_weight=>900)
+    end
+
+    canvas.rect(399, 299).styles(:fill=>'none', :stroke=>'blue')
+end
+
+rvg.draw.write('font_styles.gif')
+
---
+++ doc/ex/text01.rb
@@ -0,0 +1,18 @@
+require 'rvg/rvg'
+include Magick
+
+RVG.dpi = 90
+
+rvg = RVG.new(10.cm, 3.cm).viewbox(0,0,1000,300) do |canvas|
+    canvas.background_fill = 'white'
+    canvas.desc = "Example text01 - 'Hello, out there' in blue"
+    canvas.text(250, 150, "Hello, out there").
+                styles(:font_family=>'Verdana', :font_size=>55, :fill=>'blue')
+    canvas.circle(5, 250, 150).styles(:fill=>'red')
+
+    # Show outline of canvas using 'rect' element
+    canvas.rect(997, 297).styles(:fill=>'none', :stroke=>'blue')
+end
+
+rvg.draw.write('text01.gif')
+

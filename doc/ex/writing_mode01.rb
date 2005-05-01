119d6b6323165b54827328fd49b739b9b432e128
rmagick <rmagick>
2005-05-01 14:20:56 +0000

    Correct cut-off image on OS X

--- doc/ex/writing_mode01.rb
+++ doc/ex/writing_mode01.rb
@@ -15,13 +15,13 @@ TEXT_STYLES2 = {:writing_mode=>'tb',
                :font_weight=>'bold',
                :font_size=>16}
 
-rvg = RVG.new(1.25.in, 6.in).viewbox(0,0,125,600) do |canvas|
+rvg = RVG.new(1.25.in, 7.in).viewbox(0,0,125,700) do |canvas|
     canvas.background_fill = 'white'
 
     canvas.text(40, 15, ":glyph_orientation_vertical=0").styles(TEXT_STYLES)
     canvas.text(80, 25, ":glyph_orientation_vertical=90").styles(TEXT_STYLES2)
 
-    canvas.rect(124, 598).styles(:fill=>'none',:stroke=>'blue')
+    canvas.rect(124, 698).styles(:fill=>'none',:stroke=>'blue')
 end
 
 rvg.draw.write('writing_mode01.gif')

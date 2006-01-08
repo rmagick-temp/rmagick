87b3b9dcf5c241928a41c5d122e41bd61ea435ad
rmagick <rmagick>
2006-01-08 15:36:37 +0000

    new example for Image#vignette

---
+++ doc/ex/vignette.rb
@@ -0,0 +1,14 @@
+#! /usr/local/bin/ruby -w
+
+require 'RMagick'
+
+# Demonstrate the Image#vignette method.
+# Compare this example with the vignette.rb script in the examples directory.
+
+img = Magick::Image.read('images/Flower_Hat.jpg').first
+
+vignette = img.vignette
+
+vignette.write('vignette.jpg')
+
+exit

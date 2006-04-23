c13961ff9cccf372802f4155a2663dec10227615
rmagick <rmagick>
2006-04-23 22:28:59 +0000

    Firm up configure argument validation

--- gem_config.rb
+++ gem_config.rb
@@ -2,14 +2,24 @@
 #   to generate the top-level RMagick Makefile. Ref: rmagick.gemspec
 
 #   The gem command calls us with some extra arguments we don't use.
-#   Filter out anything that doesn't look like a configure option.
-OPTIONS = %w{ bindir sbindir  libexecdir datadir sysconfdir sharedstatedir
-              localstatedir libdir includedir oldincludedir infodir mandir
-              disable enable with without help version quiet silent cache-file
-              config-cache no-create srcdir prefix exec-prefix}.join('|')
+#   Filter out anything that isn't a configure option.
+OPTIONS = %w{ --bindir=        --sbindir=
+              --libexecdir=    --datadir=
+              --sysconfdir=    --sharedstatedir=
+              --localstatedir= --libdir=
+              --includedir=    --oldincludedir=
+              --infodir=       --mandir=
+              --disable-       --enable-
+              --with-          --without-
+              --help           --version
+              --quiet          --silent
+              --cache-file=    --config-cache
+              --no-create      --srcdir=
+              --prefix=        --exec-prefix=
+              -h -V -q -C -n }.join('|')
 
 args = []
-ARGV.each { |arg| args << arg if arg =~ /\A--(#{OPTIONS})/ }
+ARGV.each { |arg| args << arg if arg =~ /\A(#{OPTIONS})/ }
 
 cmd = "sh configure #{args.join(' ')}"
 puts "\n#{cmd}\n\n"

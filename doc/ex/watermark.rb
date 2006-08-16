beb2902ad0d3a57dbee498785d279fe1f0674f8d
rmagick <rmagick>
2006-08-16 22:18:59 +0000

    New

---
+++ doc/ex/watermark.rb
@@ -0,0 +1,23 @@
+#! /usr/local/bin/ruby -w
+
+require "RMagick"
+
+img = Magick::Image.read("images/Flower_Hat.jpg").first
+
+# Make a watermark from the word "RMagick"
+mark = Magick::Image.new(140, 40) {self.background_color = "none"}
+gc = Magick::Draw.new
+gc.annotate(mark, 0, 0, 0, -5, "RMagick") do
+    gc.gravity = Magick::CenterGravity
+    gc.pointsize = 32
+    gc.font_family = "Times"
+    gc.fill = "white"
+    gc.stroke = "none"
+end
+
+mark = mark.wave(2.5, 70).rotate(-90)
+
+# Composite the watermark in the lower right (southeast) corner.
+img2 = img.watermark(mark, 0.25, 0, Magick::SouthEastGravity)
+img2.write("watermark.jpg")
+

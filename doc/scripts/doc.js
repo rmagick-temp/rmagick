62d4194c344065ea63d32a68fb8dd09d5eb5bc46
rmagick <rmagick>
2006-08-27 23:05:21 +0000

    add addLoadEvent function

--- doc/scripts/doc.js
+++ doc/scripts/doc.js
@@ -1,9 +1,22 @@
-
 // Show an example script in a popup window
 function popup(URL) {
   day = new Date();
   id = day.getTime();
   eval("page" + id + " = window.open(URL, '" + id + "','toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1');");
+}
+
+// Run a function on window onload event
+function addLoadEvent(func) {
+    var oldonload = window.onload;
+    if (typeof window.onload != 'function') {
+        window.onload = func;
+    }
+    else {
+        window.onload = function () {
+            oldonload();
+            func();
+        }
+    }
 }
 
 

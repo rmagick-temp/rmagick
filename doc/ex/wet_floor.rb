526d35a2885ac938fa40a702526af85015f71d8b
rmagick <rmagick>
2006-12-02 19:07:17 +0000

    New example for Image#wet_floor

---
+++ doc/ex/wet_floor.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+require 'RMagick'
+
+results = Magick::ImageList.new
+
+img = Magick::Image.new(270, 60)
+
+gc = Magick::Draw.new
+gc.annotate(img, 0, 0, 0, -25, "RUBY!") do
+    gc.fill = '#c00'
+    gc.stroke = 'black'
+    gc.stroke_width = 2
+    gc.pointsize = 80
+    gc.font_weight = Magick::BoldWeight
+    gc.font_family = 'times'
+    gc.gravity = Magick::SouthGravity
+end
+
+# Add a little bit of shading
+shade = img.shade(true, 310, 30)
+img.composite!(shade, Magick::CenterGravity, Magick::HardLightCompositeOp)
+
+# Create the default reflection
+reflection = img.wet_floor
+
+ilist = Magick::ImageList.new
+ilist << img << reflection
+results << ilist.append(true)
+
+# Change the initial level of transparency and the rate of transition
+ilist[1] = img.wet_floor(0.25, 0.5)
+results << ilist.append(true)
+
+# Add a slant
+xform = Magick::AffineMatrix.new(1.0, 0.0, Math::PI/4.0, 1.0, 0.0, 0.0)
+ilist[1] = ilist[1].affine_transform(xform)
+results << ilist.append(true)
+
+# Add a ripple
+ilist[1] = ilist[1].rotate(90).wave(2, 20).rotate(-90)
+results << ilist.append(true)
+
+# Montage into a single demo image. Use a white background so
+# there won't be any problems with transparency in the browser.
+result = results.montage do
+    self.geometry = '270x120'
+    self.tile = '1x4'
+    self.background_color = 'white'
+end
+result.write('wet_floor.gif')

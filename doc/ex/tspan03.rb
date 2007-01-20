5b29153e0d958e382c9eb06fb8eb8f4ce8177005
rmagick <rmagick>
2007-01-20 15:43:27 +0000

    Fix text positioning to work better with IM 6.3.1

--- doc/ex/tspan03.rb
+++ doc/ex/tspan03.rb
@@ -7,9 +7,9 @@ rvg = Magick::RVG.new(10.cm, 3.cm).viewbox(0,0,1000,300) do |canvas|
 
     canvas.g.translate(100, 60) do |grp|
         grp.text.styles(:font_family=>'Verdana', :font_size=>45) do |txt|
-            txt.tspan("Rotation ")
-            txt.tspan("propogates ").rotate(20).styles(:fill=>'red') do |tsp|
-                tsp.tspan("to descendents").styles(:fill=>'green')
+            txt.tspan("Rotation")
+            txt.tspan(" propogates").rotate(20).styles(:fill=>'red') do |tsp|
+                tsp.tspan(" to descendents").styles(:fill=>'green')
             end
         end
     end

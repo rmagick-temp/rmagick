6a67aa3ead0efe65b18aea2484cb0483b7b4fb1f
rmagick <rmagick>
2007-01-20 01:30:35 +0000

    Fix text positioning to work better with IM 6.3.1

--- doc/ex/tspan01.rb
+++ doc/ex/tspan01.rb
@@ -6,8 +6,8 @@ rvg = Magick::RVG.new(10.cm, 3.cm).viewbox(0,0,1000,300) do |canvas|
     canvas.background_fill = 'white'
     canvas.desc = "Example tspan01 - using tspan to change visual attributes"
     canvas.g.styles(:font_family=>'Verdana', :font_size=>45) do |grp|
-        grp.text(200, 150, "You are ").styles(:fill=>'blue') do |txt|
-            txt.tspan("not").styles(:font_weight=>'bold', :fill=>'red')
+        grp.text(200, 150, "You are").styles(:fill=>'blue') do |txt|
+            txt.tspan(" not").styles(:font_weight=>'bold', :fill=>'red')
             txt.tspan(" a banana")
         end
     end
--- doc/ex/tspan02.rb
+++ doc/ex/tspan02.rb
@@ -8,8 +8,8 @@ rvg = Magick::RVG.new(10.cm, 3.cm).viewbox(0,0,1000,300) do |canvas|
     canvas.desc = "Example tspan02 - using tspan's dx and dy attributes for incremental positioning adjustments"
     canvas.g.styles(:font_family=>'Verdana', :font_size=>45) do |_g|
         _g.text(200, 150, "But you").styles(:fill=>'blue') do |txt|
-            txt.tspan("are ").d(100, -50).styles(:font_weight=>'bold', :fill=>'red')
-            txt.tspan("a peach!").d(0, 100)
+            txt.tspan("are").d(100, -50).styles(:font_weight=>'bold', :fill=>'red')
+            txt.tspan(" a peach!").d(0, 100)
         end
     end
     canvas.rect(996, 296, 1, 1).styles(:fill=>'none', :stroke=>'blue')

4d2aaa12d3dc4f3eb5febcc7b7945ecaf4ab77b9
rmagick <rmagick>
2007-02-17 15:09:02 +0000

    Test new fill=, stroke=, and undercolor= attributes

--- test/Info.rb
+++ test/Info.rb
@@ -22,4 +22,57 @@ class Info_UT < Test::Unit::TestCase
         assert_nothing_raised { @info.undefine('tiff', 'bits-per-sample') }
         assert_nil(@info['tiff', 'bits-per-sample'])
     end
+
+    def test_fill
+        assert_nothing_raised { @info.fill }
+        assert_nil(@info.fill)
+
+        assert_nothing_raised { @info.fill = 'white' }
+        assert_equal("white", @info.fill)
+
+        assert_nothing_raised { @info.fill = nil }
+        assert_nil(@info.fill)
+
+        assert_raise(ArgumentError) { @info.fill = 'xxx' }
+    end
+
+    def test_fill
+        assert_nothing_raised { @info.fill }
+        assert_nil(@info.fill)
+
+        assert_nothing_raised { @info.fill = 'white' }
+        assert_equal("white", @info.fill)
+
+        assert_nothing_raised { @info.fill = nil }
+        assert_nil(@info.fill)
+
+        assert_raise(ArgumentError) { @info.fill = 'xxx' }
+    end
+
+    def test_stroke
+        assert_nothing_raised { @info.stroke }
+        assert_nil(@info.stroke)
+
+        assert_nothing_raised { @info.stroke = 'white' }
+        assert_equal("white", @info.stroke)
+
+        assert_nothing_raised { @info.stroke = nil }
+        assert_nil(@info.stroke)
+
+        assert_raise(ArgumentError) { @info.stroke = 'xxx' }
+    end
+
+    def test_undercolor
+        assert_nothing_raised { @info.undercolor }
+        assert_nil(@info.undercolor)
+
+        assert_nothing_raised { @info.undercolor = 'white' }
+        assert_equal("white", @info.undercolor)
+
+        assert_nothing_raised { @info.undercolor = nil }
+        assert_nil(@info.undercolor)
+
+        assert_raise(ArgumentError) { @info.undercolor = 'xxx' }
+    end
+
 end

74acccac6cc36f63a024849608149d7a1e6f42a2
rmagick <rmagick>
2007-02-17 15:04:32 +0000

    Moved from doc/ex. This example is not used in the doc.

---
+++ examples/crop_with_gravity.rb
@@ -0,0 +1,46 @@
+#! /usr/local/bin/ruby -w
+
+#=======================================================#
+# Thanks to Robert Wagner for the idea of allowing a    #
+# GravityType instead of the x- and y-offset arguments! #
+#=======================================================#
+
+# Demo the use of the GravityType argument to Image#crop.
+
+require 'RMagick'
+include Magick
+
+shorts = Image.read('images/Shorts.jpg').first
+
+regwidth = shorts.columns/2
+regheight = shorts.rows/2
+
+mask = Image.new(regwidth, regheight) { self.background_color = 'white'}
+mask.opacity = 0.50 * TransparentOpacity
+
+black = Image.new(shorts.columns, shorts.rows) {self.background_color = 'black'}
+pairs = ImageList.new
+
+[NorthWestGravity, NorthGravity, NorthEastGravity,
+ WestGravity, CenterGravity, EastGravity,
+ SouthWestGravity, SouthGravity, SouthEastGravity].each do |gravity|
+    pattern = shorts.composite(mask, gravity, OverCompositeOp)
+    cropped = shorts.crop(gravity, regwidth, regheight)
+    result = black.composite(cropped, gravity, OverCompositeOp)
+    result.border_color = "white"
+    pairs << pattern
+    pairs << result
+end
+
+# Montage into a single image
+montage = pairs.montage {
+    self.geometry = "#{pairs.columns}x#{pairs.rows}+0+0"
+    self.tile = "6x3"
+    self.border_width = 1
+    }
+montage.write('crop_with_gravity.miff')
+#montage.display
+
+
+
+

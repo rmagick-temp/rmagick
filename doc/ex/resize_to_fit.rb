e70f272b81af0381f1db97cac74b1b2593fb2f14
rmagick <rmagick>
2008-01-03 23:58:16 +0000

    New

---
+++ doc/ex/resize_to_fit.rb
@@ -0,0 +1,10 @@
+#!/usr/local/bin/ruby -w
+require 'RMagick'
+
+# Demonstrate the crop_resize method
+
+img = Magick::Image.read('images/Flower_Hat.jpg')[0]
+thumbnail = img.resize_to_fit(76, 76)
+thumbnail.write("resize_to_fit.jpg")
+
+

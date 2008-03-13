5e8ac31b2653202723c0605f7286dcdc23a91edd
rmagick <rmagick>
2008-03-13 23:56:45 +0000

    Add tests for all Magick module methods

--- test/Magick.rb
+++ test/Magick.rb
@@ -7,6 +7,57 @@ require 'test/unit/ui/console/testrunner'
 
 class Magick_UT < Test::Unit::TestCase
 
+    def test_colors
+      res = nil
+      assert_nothing_raised { res = Magick.colors }
+      assert_instance_of(Array, res)
+      res.each do |c|
+        assert_instance_of(Magick::Color, c)
+        assert_instance_of(String, c.name)
+        assert_instance_of(Magick::ComplianceType, c.compliance)
+        assert_instance_of(Magick::Pixel, c.color)
+      end
+      Magick.colors {|c| assert_instance_of(Magick::Color, c) }
+    end
+
+    def test_fonts
+      res = nil
+      assert_nothing_raised { res = Magick.fonts }
+      assert_instance_of(Array, res)
+      res.each do |f|
+        assert_instance_of(Magick::Font, f)
+        assert_instance_of(String, f.name)
+        assert_instance_of(String, f.description) unless f.description.nil?
+        assert_instance_of(String, f.family)
+        assert_instance_of(Magick::StyleType, f.style)
+        assert_instance_of(Magick::StretchType, f.stretch)
+        assert_instance_of(Fixnum, f.weight)
+        assert_instance_of(String, f.encoding) unless f.encoding.nil?
+        assert_instance_of(String, f.foundry) unless f.foundry.nil?
+        assert_instance_of(String, f.format) unless f.format.nil?
+      end
+    Magick.fonts {|f| assert_instance_of(Magick::Font, f) }
+    end
+
+    def test_formats
+      res = nil
+      assert_nothing_raised { res = Magick.formats }
+      assert_instance_of(Hash, res)
+      res.each do |f, v|
+        assert_instance_of(String, f)
+        assert_instance_of(String, v)
+      end
+      Magick.formats.each { |f, v| assert_not_nil(f); assert_not_nil(v) }
+    end
+
+    def test_set_log_event_mask
+      assert_nothing_raised { Magick.set_log_event_mask("Module,Coder") }
+    end
+
+    def test_set_log_format
+      assert_nothing_raised { Magick.set_log_format("format %d%e%f") }
+    end
+
     def test_limit_resources
         cur = new = nil
 
@@ -35,6 +86,20 @@ class Magick_UT < Test::Unit::TestCase
         assert_raise(ArgumentError) { Magick::limit_resource("map", 3500, 2) }
         assert_raise(ArgumentError) { Magick::limit_resource() }
 
+    end
+
+    def test_trace_proc
+      Magick.trace_proc = lambda do |which, description, id, method|
+        assert (which == :c)
+        assert_instance_of(String, description)
+        assert_instance_of(String, id)
+        assert_equal(:initialize, method)
+      end
+      begin
+        img = Magick::Image.new(20,20)
+      ensure
+        Magick.trace_proc = nil
+      end
     end
 end
 

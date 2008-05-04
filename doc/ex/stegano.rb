66fe28c147189e4c6368f46e40b2840f3fe707e7
rmagick <rmagick>
2008-05-04 14:15:28 +0000

    Remove monitor to work around loop when running this test with GM 1.2

--- doc/ex/stegano.rb
+++ doc/ex/stegano.rb
@@ -29,11 +29,6 @@ begin
     # Read the image and retrieve the watermark. The size
     # attribute describes the size and offset of the watermark.
 
-    # This can take some time. Keep track of how far along we are.
-    # We have to be careful, though. Recent versions of ImageMagick support the
-    # Info#monitor= method and deprecate Magick.set_monitor, so don't use
-    # Magick.set_monitor if Info#monitor= is supported.
-
     monitor = Proc.new do |text, quantum, span|
         printf("Extracting watermark...%3.0f%% complete\n", ((1.0-(quantum/span.to_f))*100.0))
         true
@@ -41,17 +36,6 @@ begin
 
     stegano = Magick::Image.read('stegano:img.miff') do
         self.size = Magick::Geometry.new(wmcols, wmrows, 91)
-        begin
-            self.monitor = monitor
-        rescue NotImplementedError
-            Magick.set_monitor(monitor)
-        end
-    end
-
-    begin
-        stegano[0].monitor = nil
-    rescue NotImplementedError
-        Magick.set_monitor(nil)
     end
 
     # We don't need this any more.

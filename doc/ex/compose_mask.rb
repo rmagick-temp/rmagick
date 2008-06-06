f3334158ef4758ce30e9fbe2d5802d373ce4b5bc
rmagick <rmagick>
2008-06-06 22:38:30 +0000

    better

--- doc/ex/compose_mask.rb
+++ doc/ex/compose_mask.rb
@@ -1,8 +1,8 @@
 require 'RMagick'
 
-source = Magick::Image.read('images/Flower_Hat.jpg').first
-background = Magick::Image.read('images/Hot_Air_Balloons.jpg').first
-mask = Magick::Image.new(background.columns, background.rows)
+background = Magick::Image.read('images/Flower_Hat.jpg').first
+source = Magick::Image.read('pattern:checkerboard') {self.size = "#{background.columns}x#{background.rows}"}.first
+mask = Magick::Image.new(background.columns, background.rows) {self.background_color = "black"}
 
 # Make a mask
 gc = Magick::Draw.new
@@ -11,10 +11,13 @@ gc.annotate(mask, 0, 0, 0, 0, "Ruby") do
   gc.pointsize = 100
   gc.rotation = 90
   gc.font_weight = Magick::BoldWeight
+  gc.fill = "white"
+  gc.stroke = "none"
 end
 
 background.add_compose_mask(mask)
 result = background.composite(source, Magick::CenterGravity, Magick::OverCompositeOp)
 result.write "compose_mask_example.jpg"
-mask.write "compose_mask.jpg"
+source.write "compose_mask_source.gif"
+mask.write "compose_mask.gif"
 

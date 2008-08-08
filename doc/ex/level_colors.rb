8a84f5100196a0ad081829d0ecb7dbe9a3480670
rmagick <rmagick>
2008-08-08 22:20:10 +0000

    Handle NotImplementedError

--- doc/ex/level_colors.rb
+++ doc/ex/level_colors.rb
@@ -1,6 +1,11 @@
 require 'RMagick'
 
 img = Magick::Image.read("images/Flower_Hat.jpg").first
-img = img.level_colors("green", "orange", true)
-img.alpha(Magick::DeactivateAlphaChannel)
+begin
+  img = img.level_colors("green", "orange", true)
+  img.alpha(Magick::DeactivateAlphaChannel)
+rescue NotImplementedError
+  not_imp = Magick::Image.read('images/notimplemented.gif').first
+  img = not_imp.resize(img.columns, img.rows)
+end
 img.write("level_colors.jpg")

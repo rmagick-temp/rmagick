43479f78b94342fb8e7b9af2639b378cde5f8266
rmagick <rmagick>
2008-08-19 22:29:52 +0000

    Update test_tmpnam

--- test/Magick.rb
+++ test/Magick.rb
@@ -305,24 +305,24 @@ class Magick_UT < Test::Unit::TestCase
 
       # now it exists
       assert_nothing_raised { x = Magick._tmpnam_ }
-      assert_equal(1, Magick._tmpnam_)
+      assert_equal(tmpfiles+1, Magick._tmpnam_)
 
       info.texture = texture
-      assert_equal(2, Magick._tmpnam_)
+      assert_equal(tmpfiles+2, Magick._tmpnam_)
 
       mon = Magick::ImageList::Montage.new
       mon.texture = texture
-      assert_equal(3, Magick._tmpnam_)
+      assert_equal(tmpfiles+3, Magick._tmpnam_)
 
       mon.texture = texture
-      assert_equal(4, Magick._tmpnam_)
+      assert_equal(tmpfiles+4, Magick._tmpnam_)
 
       gc = Magick::Draw.new
       gc.composite(0, 0, 20, 20, texture)
-      assert_equal(5, Magick._tmpnam_)
+      assert_equal(tmpfiles+5, Magick._tmpnam_)
 
       gc.composite(0, 0, 20, 20, texture)
-      assert_equal(6, Magick._tmpnam_)
+      assert_equal(tmpfiles+6, Magick._tmpnam_)
 
       tmpfiles2 = Dir[ENV["HOME"] + "/tmp/magick*"].length
 

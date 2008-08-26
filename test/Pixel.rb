7e4147536dfc4df9480d86c4ea12962efaac0b7e
rmagick <rmagick>
2008-08-26 22:35:17 +0000

    Okay, we don't have to be quite so thorough when testing to_/from_hsla.

--- test/Pixel.rb
+++ test/Pixel.rb
@@ -60,11 +60,11 @@ class Pixel_UT < Test::Unit::TestCase
       assert_raise(TypeError) { Magick::Pixel.from_hsla(127, 50, 50, []) }
       assert_nothing_raised { @pixel.to_hsla }
 
-      180.times do |h|
+      45.times do |h|
         25.times do |s|
           25.times do |l|
             5.times do |a|
-              args = [2*h, s+25, l+25, a/5]
+              args = [8*h, s+25, l+25, a/5]
               px = Magick::Pixel.from_hsla(*args)
               hsla = px.to_hsla()
               #puts "[#{args.join(', ')}] = [#{hsla.join(', ')}]"

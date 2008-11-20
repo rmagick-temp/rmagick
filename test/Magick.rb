4ed02e9681c2b28992d1a34590a57e8f3e3eec6d
rmagick <rmagick>
2008-11-20 00:08:34 +0000

    Update test_tmpfiles test

--- test/Magick.rb
+++ test/Magick.rb
@@ -293,9 +293,11 @@ class Magick_UT < Test::Unit::TestCase
     # test the @@_tmpnam_ class variable
     # the count is incremented by Image::Info#texture=,
     # ImageList::Montage#texture=, and Draw.composite
+
     def test_tmpnam
 
       tmpfiles = Dir[ENV["HOME"] + "/tmp/magick*"].length
+
       texture = Magick::Image.read("granite:") {self.size = "20x20" }.first
       info = Magick::Image::Info.new
 
@@ -305,24 +307,24 @@ class Magick_UT < Test::Unit::TestCase
 
       # now it exists
       assert_nothing_raised { x = Magick._tmpnam_ }
-      assert_equal(tmpfiles+1, Magick._tmpnam_)
+      assert_equal(1, Magick._tmpnam_)
 
       info.texture = texture
-      assert_equal(tmpfiles+2, Magick._tmpnam_)
+      assert_equal(2, Magick._tmpnam_)
 
       mon = Magick::ImageList::Montage.new
       mon.texture = texture
-      assert_equal(tmpfiles+3, Magick._tmpnam_)
+      assert_equal(3, Magick._tmpnam_)
 
       mon.texture = texture
-      assert_equal(tmpfiles+4, Magick._tmpnam_)
+      assert_equal(4, Magick._tmpnam_)
 
       gc = Magick::Draw.new
       gc.composite(0, 0, 20, 20, texture)
-      assert_equal(tmpfiles+5, Magick._tmpnam_)
+      assert_equal(5, Magick._tmpnam_)
 
       gc.composite(0, 0, 20, 20, texture)
-      assert_equal(tmpfiles+6, Magick._tmpnam_)
+      assert_equal(6, Magick._tmpnam_)
 
       tmpfiles2 = Dir[ENV["HOME"] + "/tmp/magick*"].length
 

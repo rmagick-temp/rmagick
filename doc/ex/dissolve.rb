0f981341341684a5a4b7bcff004558a446bbc886
rmagick <rmagick>
2009-07-23 22:24:33 +0000

    Change crop_resized! to resize_to_fill!

--- doc/ex/dissolve.rb
+++ doc/ex/dissolve.rb
@@ -6,7 +6,7 @@ bgnd = Magick::Image.read('images/Violin.jpg').first
 overlay = Magick::Image.read('images/Flower_Hat.jpg').first
 
 # Make the violin image the same size as the hat image
-bgnd.crop_resized!(overlay.columns, overlay.rows)
+bgnd.resize_to_fill!(overlay.columns, overlay.rows)
 
 composited = bgnd.dissolve(overlay, 0.50)
 composited.write('dissolve.jpg')
